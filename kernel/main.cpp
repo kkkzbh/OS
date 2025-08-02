0f3c41d498a60dd0216be8cf283860818948cee0
kkkzbh <Uika@foxmail.com>
2025-08-03 01:31:18 +0800

    初步引入线程 - 线程模块 thread - 进行简单的线程调用
    发现BUG
    - 在C++环境下使用列表初始化会造成虚拟机卡死

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 import memory;
-
+import thread;
 
 extern "C" auto start() -> void
 {
@@ -15,6 +15,21 @@ extern "C" auto start() -> void
     puts("get_kernel_page start vaddr is: ");
     puthex(reinterpret_cast<u32>(addr));
     putchar('\n');
+
+    auto v = reinterpret_cast<int*>(addr);
+    *v = 123;
+    puts("modify the kernel memory : v = ");
+    puthex(*v);
+    putchar('\n');
+
+    thread_start("kthread_a",31,
+        [](void* arg) -> void {
+            auto s = static_cast<char*>(arg);
+            while(1) {
+                puts(s);
+            }
+        }
+        ,const_cast<char*>("Arg666   "));
 
     while(true) {
 
