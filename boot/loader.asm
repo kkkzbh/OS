89738629555cf878c4f24de3438cee980c2a6b64
kkkzbh <Uika@foxmail.com>
2025-08-04 22:32:38 +0800

    将内核改为读取250个扇区，应对越来越大的内核

--- boot/loader.asm
+++ boot/loader.asm
@@ -149,7 +149,7 @@ p_mode_start:
 
     mov eax, KERNEL_START_SECTOR    ; kernel.bin所在的扇区号
     mov ebx, KERNEL_BIN_BASE_ADDR   ; 磁盘读出后写入内存的起始地址
-    mov ecx, 200                    ; 读取的扇区数
+    mov ecx, 250                    ; 读取的扇区数
 
     call rd_disk_m_32
     
