1e5eb90fd56496251875b611c9d9baf08595c13c
kkkzbh <Uika@foxmail.com>
2025-08-22 18:03:49 +0800

    简单优化一些文件布局，使kernel的起点main函数看起来更舒适

---
+++ kernel/kernel.cpp
@@ -0,0 +1,19 @@
+
+
+export module kernel;
+
+export import console;
+export import thread;
+export import process;
+export import sys;
+export import syscall.init;
+export import printf;
+export import getpid;
+export import malloc;
+export import list;
+export import free;
+export import format;
+export import string;
+export import string.format;
+export import array;
+export import array.format;
