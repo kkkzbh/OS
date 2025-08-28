2df7ed8e4ee5fe8146d0046ee71c3a606b822e47
kkkzbh <kkkzbh@k.com>
2025-08-29 02:40:10 +0800

    实现inode_open与inode_close对应文件开关时候的inode操作

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -10,30 +10,4 @@ using namespace console;
 auto main() -> void
 {
 
-
-    auto a = std::vector<int>{};
-    a.emplace_back(1);
-    a.emplace_back(4);
-    a.emplace_back(7);
-
-    auto r = a | decorate[([](auto& v) -> auto& {
-        ++v;
-        return v;
-    })] | first[([](auto v) {
-        return v % 2 == 0;
-    })];
-
-
-    println("{}",a);
-
-    static_assert(std::same_as<decltype(*r.it),int&>);
-
-    auto y = r.get();
-
-    r.get() = 10;
-
-    println("{}",a);
-
-
-
 }
