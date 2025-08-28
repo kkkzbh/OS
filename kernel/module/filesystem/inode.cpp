d8be0012fee614993aa772f9d59187bd9c356e8a
kkkzbh <kkkzbh@k.com>
2025-08-29 02:40:02 +0800

    实现inode_open与inode_close对应文件开关时候的inode操作

--- kernel/module/filesystem/inode.cpp
+++ kernel/module/filesystem/inode.cpp
@@ -2,6 +2,7 @@ module;
 
 #include <assert.h>
 #include <string.h>
+#include <interrupt.h>
 
 export module inode;
 
@@ -11,16 +12,22 @@ import array;
 import ide;
 import filesystem.utility;
 import algorithm;
+import schedule;
+import vector;
 
 export struct inode
 {
+    constexpr inode() = default;
+
+    constexpr inode(size_t n) : no(n) {}
+
     u32 no;                 // inode编号
-    u32 size;               // 文件指文件大小，目录指所有目录项的大小之和
+    u32 size = 0;               // 文件指文件大小，目录指所有目录项的大小之和
 
-    u32 open_cnts;          // 记录文件被打开的次数
-    bool wdeny;             // 写文件不能并行，写时要检查该标识
+    u32 open_cnts = 0;          // 记录文件被打开的次数
+    bool wdeny = false;             // 写文件不能并行，写时要检查该标识
 
-    std::array<u32,13> sectors; // [0-11]直接块 12存一级间接块指针 (二三级间接块指针暂不实现)
+    std::array<u32,13> sectors{}; // [0-11]直接块 12存一级间接块指针 (二三级间接块指针暂不实现)
     list::node tag;         // 用于加入已打开的inode列表，主要用于缓存文件
 };
 
@@ -74,13 +81,54 @@ auto inode_sync(partition* part,inode* node,void* buf) -> void
 // 根据i结点号返回相应的i结点
 auto inode_open(partition* part,u32 inode_no) -> inode*
 {
-    // auto& il = part->open_inodes;
-    // auto inode_bylist = [](list::node& nd) -> inode* {
-    //     return (inode*)((u32)&nd - (u32)(&static_cast<inode*>(0)->tag));
-    // };
-    // auto elem = il | std::decorate[inode_bylist] | std::first[([=](inode* ind) {
-    //     return ind->no == inode_no;
-    // })];
+    auto& il = part->open_inodes;
+    auto node {
+        il
+        | std::decorate[([](list::node& nd) -> inode* {
+            return (inode*)((u32)&nd - (u32)(&static_cast<inode*>(0)->tag));
+        })]
+        | std::first[([=](inode* ind) {
+            return ind->no == inode_no;
+        })]
+    };
+    if(node) {
+        return node.get();
+    }
 
+    // 如果在open_inodes中找不到，就从硬盘上读入此inode并加入到open_inodes链表
+    auto inode_pos = inode_locate(part,inode_no);
 
+    // 需要让新inode被所有任务共享，让inode置于内核空间，于是需要临时将cur_pbc->pgdir置为nullptr
+    auto cur = running_thread();
+    auto cur_pagedir_bak = std::exchange(cur->pgdir,nullptr);
+    // 这样分配的nd将源自内核空间
+    auto nd = new inode;
+    // 恢复pgdir
+    cur->pgdir = cur_pagedir_bak;
+    auto seccnt = 1 + inode_pos.two_sec;
+    auto buf = std::vector(seccnt * BLOCK_SIZE,'\0');
+    ide_read(part->my_disk,inode_pos.sec_lba,buf.data(),seccnt);
+    memcpy(nd,buf.data() + inode_pos.off_size,sizeof(inode));
+
+    // 在缓存缺失的状态下,从硬盘载入inode,这说明马上要用到inode,插入到队首
+    il.push_front(&nd->tag);
+    nd->open_cnts = 1;
+    return nd;
+}
+
+// 关闭inode或减少inode的打开数
+auto inode_close(inode* node) -> void
+{
+    auto old_status = intr_disable();
+    // 如果关闭后，已经没有进程打开这个文件，就去掉该inode，把空间释放
+    if(--node->open_cnts == 0) {
+        list::erase(&node->tag);
+        // 同样的套路，从内核空间回收内存
+        auto cur = running_thread();
+        auto bak = std::exchange(cur->pgdir,nullptr);
+        cur->pgdir = nullptr;
+        delete node;
+        cur->pgdir = bak;
+    }
+    intr_set_status(old_status);
 }
