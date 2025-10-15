e552a6ce80daae5535879b2248082424483569dc
kkkzbh <kkkzbh@k.com>
2025-10-15 17:05:16 +0800

    实现创建目录函数(但有BUG，记录状态，解决中)

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -30,4 +30,31 @@ auto main() -> void
     auto ok = unlink("/file3");
     console::println("unline /file3: state {}",ok ? "yes" : "no");
 
+    auto dir = "/kkkzbh/code";
+    ok = mkdir(dir);
+    console::println("the dir {} create {}",dir,ok ? "successful!" : "failed!");
+    auto dir2 = "/kkkzbh/";
+    ok = mkdir(dir2);
+    console::println("the dir {} create {}",dir2,ok ? "successful!" : "failed!");
+    if(not ok) {
+        auto dir = "/kkkzbh";
+        ok = mkdir(dir);
+        console::println("the dir {} create {}",dir,ok ? "successful!" : "failed!");
+    }
+    auto dir3 = "/kkkzbh/files";
+    ok = mkdir(dir3);
+    console::println("the dir {} create {}",dir3,ok ? "successful!" : "failed!");
+    fd = open("/kkkzbh/files/file1",+open_flags::create | +open_flags::rdwr).value_or(-1);
+    if(fd == -1) {
+        console::println("open /kkkzbh/files/file1 failed!");
+    } else {
+        console::println("open the /kkkzbh/files/file1 successful!");
+        auto word = "I can create the directory, yeah!\n"sv;
+        write(fd,word.data(),word.size());
+        lseek(fd,0,whence::set);
+        auto a = std::array<char,128>{};
+        read(fd,a.data(),word.size());
+        console::println("I catch the /kkkzbh/files/file1, it is \n{}",a);
+        close(fd);
+    }
 }
