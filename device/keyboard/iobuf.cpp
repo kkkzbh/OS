43fd201ae4bd79c0cfa193fb9aafe0e352da5f7e
kkkzbh <Uika@foxmail.com>
2025-10-18 01:17:32 +0800

    取消键盘中断引起回显的机制，修复write函数针对stdout使用println而不是print导致多一个换行的问题

--- device/keyboard/iobuf.cpp
+++ device/keyboard/iobuf.cpp
@@ -11,7 +11,7 @@ export auto ioqbuf = ioqueue{};
 extern "C" auto solve(char c) -> void
 {
     if(not ioqbuf.full()) { // 如果键盘缓冲区没满
-        putchar(c);
+        // putchar(c);  // 不回显，仅测试中断用，应该由shell来控制是否回显
         ioqbuf.put(c);
     }
 }
